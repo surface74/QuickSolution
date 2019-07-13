4877cb079623548ab911fb1dde1c53b1ad0441bb
User <User@VPC-202068>
2019-07-13 23:26:51 +0300

    new test

---
+++ QuickTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("QuickTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("QuickTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("ec1a0ccb-f9f9-4863-be98-c12065dac39d")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ QuickTest/UnitTest1.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub TestGetCurrentDate()
+        Assert.AreEqual(DateTime.Now.Date, QuickDate.Calendar.GetCurrentDate())
+    End Sub
+
+End Class
